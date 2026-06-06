99d952e9f375052f056148f7507d61054ec81c55
bitkids <kuroobicode@gmail.com>
2026-06-07 00:03:18 +0900

    add backup

---
+++ sound/06/back_tanoshimi-dev.rb
@@ -0,0 +1,223 @@
+use_bpm 125
+
+
+mo_jumpy_flip_fsm = "E:/sound/splice/Samples/packs/Golden Axe, a fortuneswan moment/Moment_-_Golden_Axe__a_fortuneswan_moment/loops/gen_loops/MO_FS_130_guitar_loop_jumpy/MO_FS_130_electric_guitar_loop_jumpy_flip_F#min.wav"
+mo_jumpy_lead_fsm = "E:/sound/splice/Samples/packs/Golden Axe, a fortuneswan moment/Moment_-_Golden_Axe__a_fortuneswan_moment/loops/gen_loops/MO_FS_130_guitar_loop_jumpy/MO_FS_130_electric_guitar_loop_jumpy_lead_F#min.wav"
+
+
+mo_dt_122_fs = "E:/sound/splice/Samples/packs/droplets, a demotapes moment/Moment_demotapes_droplets/loops/songstarters/MO_DT_122_songstarter_triumph_sample_chop_F#maj.wav"
+
+# samples ###############################################################
+dr1 = "E:/sound/splice/Samples/packs/Holychild's Snack Pack/HOLYCHILD_sample_pack/HOLYCHILD_drums/HOLYCHILD_beats/HOLYCHILD_drum_loop_brushes_folk_groove_125.wav"
+
+dr2 = "E:/sound/splice/Samples/packs/Travis Barker Drum Kit/TRAVIS_BARKER_sample_pack/TRAVIS_BARKER_drums/TRAVIS_BARKER_drum_loops/TRAVIS_BARKER_drum_loop_break_29_beat_groove_140.wav"
+dr3 = "E:/sound/splice/Samples/packs/Sullivan King Metal Electronic/SLK_sample_pack/SLK_drums/SLK_drum_fills/SLK_drum_fill_anaconda_150.wav"
+
+gt1 = "E:/sound/splice/Samples/packs/Indie Rock and Electronica/SM196_-_Indie_Rock___Electronica_-_Splice/inspiration_loops/125/ire_insp_125_fire_A/ire_insp_125_fire_full_A.wav"
+gt2 = "E:/sound/splice/Samples/packs/Indie Rock and Electronica/SM196_-_Indie_Rock___Electronica_-_Splice/inspiration_loops/125/ire_insp_125_bed_A/ire_insp_125_bed_guitar3_A.wav"
+
+syn1 = "E:/sound/splice/Samples/packs/Indie Rock and Electronica/SM196_-_Indie_Rock___Electronica_-_Splice/guitar_loops/125/ire_guitar_125_rev2_A.wav"
+syn_pad = "E:/sound/splice/Samples/packs/Indie Rock and Electronica/SM196_-_Indie_Rock___Electronica_-_Splice/pad___atmosphere_loops/125/ire_atmos_125_dream2_A.wav"
+
+bs1 = "E:/sound/splice/Samples/packs/Indie Rock and Electronica/SM196_-_Indie_Rock___Electronica_-_Splice/bass_loops/125/ire_bass_125_drive_A.wav"
+
+#trktrn_arp_a = "E:/sound/splice/Samples/packs/Indie Beats Sample Pack by Prod Kult x RFM Beats/Traktrain_-_Indie_Beats_Sample_Pack_by_Prod_Kult_x_RFM_Beats/Loops/Kit_Loops/Good_Times/TRKTRN_IBSPBPKXRB_139_Synth_Arp_Good_Times_Amaj.wav"
+trktrn_arp_a_bpm125 = "E:/sound/splice/Samples/packs/Indie Beats Sample Pack by Prod Kult x RFM Beats/Traktrain_-_Indie_Beats_Sample_Pack_by_Prod_Kult_x_RFM_Beats/Loops/Kit_Loops/Good_Times/TRKTRN_IBSPBPKXRB_139_Synth_Arp_Good_Times_Amaj_bpm125.wav"
+
+
+fx1 = "E:/sound/splice/Samples/packs/Indie Rock and Electronica/SM196_-_Indie_Rock___Electronica_-_Splice/inspiration_loops/125/ire_insp_125_time_D/ire_insp_125_time_full_D.wav"
+fx2_joy = "E:/sound/splice/Samples/packs/Indie Rock and Electronica/SM196_-_Indie_Rock___Electronica_-_Splice/pad___atmosphere_loops/125/ire_atmos_125_joy4_G.wav"
+
+
+ip_syn123_scanners_e_bpm125 = "E:/sound/splice/Samples/packs/Indie Pop/SM83_-_Indie_Pop_-_Wav/synth_loops/123/ip_syn123_scanners_E_bpm125.wav"
+ip_syn123_grove_e = "E:/sound/splice/Samples/packs/Indie Pop/SM83_-_Indie_Pop_-_Wav/synth_loops/123/ip_syn123_grove_E.wav"
+
+ip_syn123_magic_gs_bpm125 = "E:/sound/splice/Samples/packs/Indie Pop/SM83_-_Indie_Pop_-_Wav/synth_loops/123/ip_syn123_magic_G#_bpm125.wav"
+
+ip_syn123_fallout_gm_bpm125 = "E:/sound/splice/Samples/packs/Indie Pop/SM83_-_Indie_Pop_-_Wav/synth_loops/123/ip_syn123_fallout_Gm_bpm125.wav"
+
+ip_syn123_arp_a_bpm125 = "E:/sound/splice/Samples/packs/Indie Pop/SM83_-_Indie_Pop_-_Wav/synth_loops/123/ip_syn123_arp_A_bpm125.wav"
+
+trktrn_124_bm = "E:/sound/splice/Samples/packs/Indie Beats Sample Pack by Prod Kult x RFM Beats/Traktrain_-_Indie_Beats_Sample_Pack_by_Prod_Kult_x_RFM_Beats/Loops/Kit_Loops/Flowers/TRKTRN_IBSPBPKXRB_124_Synth_Lead_Flowers_Bmin.wav"
+
+trjtrn_syn_piano_a_bpm125 = "E:/sound/splice/Samples/packs/Indie Beats Sample Pack by Prod Kult x RFM Beats/Traktrain_-_Indie_Beats_Sample_Pack_by_Prod_Kult_x_RFM_Beats/Loops/Kit_Loops/Beautiful/TRKTRN_IBSPBPKXRB_92_Synth_Electric_Piano_Beautiful_Amaj_bpm125.wav"
+
+trjtrn_rareware_a_bpm125 = "E:/sound/splice/Samples/packs/Indie Beats Sample Pack by Prod Kult x RFM Beats/Traktrain_-_Indie_Beats_Sample_Pack_by_Prod_Kult_x_RFM_Beats/Loops/Kit_Loops/Rarewave/TRKTRN_IBSPBPKXRB_94_Songstarter_Rarewave_Amaj_bpm125.wav"
+
+# intro ###############################################################
+#amp_intro_dr1 = 2
+#sample dr1 ,amp: amp_intro_dr1 ,beat_stretch: 32, finish: 0.5
+#sleep 16
+
+
+
+# guitar ###############################################################
+amp_gt1 = 2
+#amp_gt1 = 0
+live_loop :gt1 do
+  #sample :drum_cymbal_open ,amp: amp_gt1 * 0.25
+  sample gt1 ,amp: amp_gt1 ,beat_stretch: 16
+  sleep 16
+  sample gt1 ,amp: amp_gt1 ,beat_stretch: 16
+  sleep 16
+end
+
+amp_syn2 = 1
+#amp_syn2 = 0
+live_loop :syn2 do
+  sample syn_pad ,amp: amp_syn2 ,beat_stretch: 8
+  sleep 8
+  sample syn_pad ,amp: amp_syn2 ,beat_stretch: 16
+  sleep 8
+end
+
+
+
+# 2nd  ###############################################################
+amp_trktrn_arp_a_bpm125 = 0.5
+amp_trktrn_arp_a_bpm125 = 0
+live_loop :trktrn_arp_a_bpm125 do
+  sample trktrn_arp_a_bpm125, amp: amp_trktrn_arp_a_bpm125, finish: 0.5
+  sleep 32
+end
+
+amp_trjtrn_rareware_a_bpm125 = 1
+amp_trjtrn_rareware_a_bpm125 = 0
+live_loop :trjtrn_rareware_a_bpm125 do
+  sample trjtrn_rareware_a_bpm125, amp: amp_trjtrn_rareware_a_bpm125
+  sleep 16
+  sample trjtrn_rareware_a_bpm125, amp: amp_trjtrn_rareware_a_bpm125
+  sleep 16
+end
+
+# 3nd  ###############################################################
+amp_trjtrn_syn_piano_a_bpm125 = 0.75
+amp_trjtrn_syn_piano_a_bpm125 = 0
+live_loop :trjtrn_syn_piano_a_bpm125 do
+  sample trjtrn_syn_piano_a_bpm125, amp: amp_trjtrn_syn_piano_a_bpm125
+  sleep 16
+  sample trjtrn_syn_piano_a_bpm125, amp: amp_trjtrn_syn_piano_a_bpm125
+  sleep 16
+end
+
+amp_trktrn_124_bm = 0.5
+amp_trktrn_124_bm = 0
+live_loop :trktrn_124_bm do
+  sample trktrn_124_bm, amp: amp_trktrn_124_bm
+  sleep 16
+  sample trktrn_124_bm, amp: amp_trktrn_124_bm
+  sleep 16
+end
+
+# drum ###############################################################
+amp_dr1 = 2
+#amp_dr1 = 0
+live_loop :dr1 do
+  sample dr1 ,amp: amp_dr1 ,beat_stretch: 32
+  sleep 32
+end
+
+
+
+# synth ###############################################################
+amp_syn1 = 1
+amp_syn1 = 0
+live_loop :syn1 do
+  sample syn1 ,amp: amp_syn1 ,beat_stretch: 8
+  sleep 8
+  sample syn1 ,amp: amp_syn1 ,beat_stretch: 8
+  sleep 8
+  sample syn1 ,amp: amp_syn1 ,beat_stretch: 8
+  sleep 8
+  sample syn1 ,amp: amp_syn1 ,beat_stretch: 8
+  sleep 8
+end
+
+amp_fx1 = 1.5
+amp_fx1 = 0
+live_loop :fx1  do
+  sample fx1 ,amp: amp_fx1 ,beat_stretch: 32
+  sleep 32
+end
+
+amp_gt2 = 0.25
+amp_gt2 = 0
+live_loop :gt2 do
+  sleep 16
+  sample gt2 ,amp: amp_gt2 ,beat_stretch: 16
+  sleep 16
+end
+
+
+# 3nd  ###############################################################
+
+amp_ip_syn123_fallout_gm_bpm125 = 1
+amp_ip_syn123_fallout_gm_bpm125 = 0
+live_loop :ip_syn123_fallout_gm_bpm125 do
+  sample ip_syn123_fallout_gm_bpm125, amp: amp_ip_syn123_fallout_gm_bpm125
+  sleep 16
+end
+
+amp_ip_syn123_scanners_e_bpm125 = 0.5
+amp_ip_syn123_scanners_e_bpm125 = 0
+live_loop :ip_syn123_scanners_e_bpm125 do
+  sample ip_syn123_scanners_e_bpm125 ,amp: amp_ip_syn123_scanners_e_bpm125
+  sleep 32
+end
+
+amp_ip_syn123_arp_a_bpm125 = 0.25
+amp_ip_syn123_arp_a_bpm125 = 0
+live_loop :ip_syn123_arp_a_bpm125 do
+  sample ip_syn123_arp_a_bpm125, amp: amp_ip_syn123_arp_a_bpm125
+  sleep 16
+end
+
+
+# ending ###############################################################
+
+amp_ip_syn123_magic_gs_bpm125 = 1
+amp_ip_syn123_magic_gs_bpm125 = 0
+live_loop :ip_syn123_magic_gs_bpm125 do
+  sample ip_syn123_magic_gs_bpm125, amp: amp_ip_syn123_magic_gs_bpm125
+  sleep 32
+end
+
+
+
+# not used ###############################################################
+amp_bs1 = 1
+amp_bs1 = 0
+live_loop :bs1 do
+  sample bs1 ,amp: amp_bs1 ,beat_stretch: 8
+  sleep 8
+end
+
+amp_mo_dt_122_fs = 2
+amp_mo_dt_122_fs = 0
+live_loop :mo_dt_122_fs do
+  sample mo_dt_122_fs ,amp: amp_mo_dt_122_fs
+  sleep 16
+end
+
+
+amp_mo_jumpy_flip_fsm = 2
+amp_mo_jumpy_flip_fsm = 0
+live_loop :mo_jumpy_flip_fsm do
+  sample mo_jumpy_flip_fsm ,amp: amp_mo_jumpy_flip_fsm
+  sleep 16
+end
+
+amp_mo_jumpy_lead_fsm = 2
+amp_mo_jumpy_lead_fsm = 0
+live_loop :mo_jumpy_lead_fsm do
+  sample mo_jumpy_lead_fsm ,amp: amp_mo_jumpy_lead_fsm
+  sleep 16
+end
+
+amp_dr2 = 1
+amp_dr2 = 0
+live_loop :dr2 do
+  sample :drum_cymbal_soft ,amp: amp_dr2
+  sleep 1
+end
+
