ecf018ddf025f2bf347e460d94a9b24b47350b02
bitkids <kuroobicode@gmail.com>
2026-06-07 18:21:13 +0900

    update

--- sound/06/0607_shoegaze.rb
+++ sound/06/0607_shoegaze.rb
@@ -5,12 +5,11 @@ use_bpm 89
 
 trktrn_awaken_fsm = "E:/sound/splice/Samples/packs/Common Drift Loop Kit by Noria/Traktrain_-_Common_Drift_Loop_Kit_by_Noria/Loops/Kit_Loops/Awaken/TRKTRN_CDLKBN_89_Electric_Guitar_Rhythm_Awaken_Clean_Dry_F#min.wav"
 trktrn_glitch_fsm = "E:/sound/splice/Samples/packs/Common Drift Loop Kit by Noria/Traktrain_-_Common_Drift_Loop_Kit_by_Noria/Loops/Kit_Loops/Awaken/TRKTRN_CDLKBN_89_Electric_Guitar_Glitch_Awaken_F#min.wav"
-
-
 trktrn_stack_fsm = "E:/sound/splice/Samples/packs/Common Drift Loop Kit by Noria/Traktrain_-_Common_Drift_Loop_Kit_by_Noria/Loops/Kit_Loops/Awaken/TRKTRN_CDLKBN_89_Electric_Guitar_Stack_Awaken_Clean_Wet_F#min.wav"
 
+trktrn_dysthymia_bm = "E:/sound/splice/Samples/packs/Common Drift Loop Kit by Noria/Traktrain_-_Common_Drift_Loop_Kit_by_Noria/Loops/Kit_Loops/Dysthymia/TRKTRN_CDLKBN_105_Distorted_Electric_Guitar_Rhythm_Dysthymia_Bmin_bpm89.wav"
+trktrn_dysthymia_clean_bm = "E:/sound/splice/Samples/packs/Common Drift Loop Kit by Noria/Traktrain_-_Common_Drift_Loop_Kit_by_Noria/Loops/Kit_Loops/Dysthymia/TRKTRN_CDLKBN_105_Electric_Guitar_Rhythm_Dysthymia_Clean_Dry_Bmin_bpm89.wav"
 nh_esg_chapter_e = "E:/sound/splice/Samples/packs/my head has turned to noise...Explorations in Shoegaze/Noise_Honey_-_my_head_has_turned_to_noise...Explorations_in_Shoegaze_-_WAV/loops/kits/NH_ESG_90_chapter_Emaj/NH_ESG_90_bass_electric_fuzz_chapter_Emaj_bpm89.wav"
-
 
 ne_esg_cordial_a = "E:/sound/splice/Samples/packs/my head has turned to noise...Explorations in Shoegaze/Noise_Honey_-_my_head_has_turned_to_noise...Explorations_in_Shoegaze_-_WAV/loops/kits/NH_ESG_150_cordial_Amaj/NH_ESG_150_songstarter_cordial_Amaj_bpm89.wav"
 ne_esg_ambient_d = "E:/sound/splice/Samples/packs/my head has turned to noise...Explorations in Shoegaze/Noise_Honey_-_my_head_has_turned_to_noise...Explorations_in_Shoegaze_-_WAV/loops/guitar/electric/NH_ESG_150_guitar_electric_ambient_sourpatch_Dmaj_bpm89.wav"
@@ -34,8 +33,13 @@ live_loop :trktrn_glitch_fsm do
   sleep 32
 end
 
-
 #  ###############################################################
+amp_trktrn_dysthymia_clean_bm = 1
+amp_trktrn_dysthymia_clean_bm = 0
+live_loop :trktrn_dysthymia_clean_bm do
+  sample trktrn_dysthymia_clean_bm ,amp: amp_trktrn_dysthymia_clean_bm, finish: 0.125
+  sleep 32
+end
 
 amp_nh_esg_chapter_e = 1.5
 amp_nh_esg_chapter_e = 0
@@ -60,7 +64,7 @@ live_loop :trktrn_stack_fsm do
 end
 
 amp_ne_esg_ambient_d = 0.75
-amp_ne_esg_ambient_d = 0
+#amp_ne_esg_ambient_d = 0
 live_loop :ne_esg_ambient_d do
   sample ne_esg_ambient_d ,amp: amp_ne_esg_ambient_d
   sleep 32
@@ -68,7 +72,7 @@ end
 
 #  ###############################################################
 amp_shs_torment = 1
-amp_shs_torment = 0
+#amp_shs_torment = 0
 live_loop :shs_torment do
   sample shs_torment ,amp: amp_shs_torment, beat_stretch: 16
   sleep 16
